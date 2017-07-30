bbfc0e84fb507b7b7f5cdfe301185e72a410b6fa
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-07-30 21:21:39 +0200

    Make movement allower use the default calculations

--- src/main/java/alexanders/mods/pof/event/MovementAllower.java
+++ src/main/java/alexanders/mods/pof/event/MovementAllower.java
@@ -8,21 +8,16 @@ import de.ellpeck.rockbottom.api.event.impl.ResetMovedPlayerEvent;
 public class MovementAllower implements IEventListener<ResetMovedPlayerEvent> { //Awful name I know
     @Override
     public EventResult listen(EventResult result, ResetMovedPlayerEvent event) {
+        
         DataSet data = event.player.getAdditionalData();
         if (data != null) {
-            double xMoved = event.player.x - event.lastCalcX;
-            double yMoved = event.player.y - event.lastCalcY;
-            double movedXAllowed = Math.abs(event.allowedDefaultDistance);
-            double movedYAllowed = Math.abs(event.allowedDefaultDistance);
             int speedLeft;
             if ((speedLeft = data.getInt("effectSpeedActive")) > 0) {
-                movedXAllowed += 0.1 * (250 - speedLeft);
+                event.allowedDefaultDistance += 0.1 * (250 - speedLeft);
             }
             if (data.getInt("effectJumpActive") > 0) {
-                movedYAllowed += 1.5;
+                event.allowedDefaultDistance += 1.5;
             }
-            if (Math.abs(movedXAllowed) > Math.abs(xMoved) || Math.abs(movedYAllowed) > Math.abs(yMoved))
-                return EventResult.CANCELLED;
         }
         return result;
     }
